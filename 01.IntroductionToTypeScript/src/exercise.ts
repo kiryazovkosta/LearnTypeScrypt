76e5eb32cf7d8712267027f9407766e13733b69a
kiryazovkosta <kosta.kiryazov@gmail.com>
2025-05-13 15:29:53 +0300

    Add Introduction to TypeScript exercise

--- 01.IntroductionToTypeScript/src/exercise.ts
+++ 01.IntroductionToTypeScript/src/exercise.ts
@@ -2,4 +2,22 @@ export class Exercises {
     evenSum(a: number, b: number, c: number) : boolean  {
         return (a + b + c) % 2 == 0
     }
+
+    dayOfTheWeek(day: number): string {
+        enum WeekDays {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+        }
+
+        if (day >= WeekDays.Monday && day <= WeekDays.Sunday) {
+            return WeekDays[day];
+        }
+
+        return 'error'
+    }
 } 
--- 01.IntroductionToTypeScript/src/index.ts
+++ 01.IntroductionToTypeScript/src/index.ts
@@ -2,8 +2,20 @@ import { Exercises } from "./exercise";
 
 console.log('Intro to TypeScript');
 
+/* 1. Even Sum */
 const exercises = new Exercises();
 let isEven = exercises.evenSum(1, 2, 3);
 console.log(isEven)
 isEven = exercises.evenSum(2, 2, 3)
 console.log(isEven);
+
+/* 2. Day of the Week */
+let day = 1;
+let dayAsString = exercises.dayOfTheWeek(day);
+console.log(dayAsString);
+day = 5;
+dayAsString = exercises.dayOfTheWeek(day);
+console.log(dayAsString);
+day = -1;
+dayAsString = exercises.dayOfTheWeek(day);
+console.log(dayAsString);
