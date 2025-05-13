579a4bf4bf3353440554b7a4e70f9e3db0575fb5
kiryazovkosta <kosta.kiryazov@gmail.com>
2025-05-13 22:42:16 +0300

    Add Intro to TypeScript exercises

--- 01.IntroductionToTypeScript/src/exercise.ts
+++ 01.IntroductionToTypeScript/src/exercise.ts
@@ -17,4 +17,40 @@ export class Exercises {
         const dayName = WeekDays[day];
         return dayName !== undefined ? dayName : 'error';
     }
+
+    formatPerson(person:[string, number]): string {
+        const [name, age] = person;
+        return `Hello, my name is ${name} and my age is ${age}`;
+    }
+
+    convertArray(arr: string[]): [string, number] {
+        const concatenatedString = arr.join('');
+        return [concatenatedString, concatenatedString.length];
+    }
+
+    summarizePerson(id: number, firstName: string, lastName: string, age: number, 
+        middleName?:string, hobbies?:string[], workInfo?:[string,number])
+        : [number, string, number, string|string[], string] {
+        
+        const fullName: string = middleName ? `${firstName} ${middleName} ${lastName}` : `${firstName} ${lastName}`;
+        const hobbiesString = hobbies ? hobbies.join(', ') : '-';
+        const workInfoString = workInfo ? `${workInfo[0]} -> ${workInfo[1]}` : '-';
+        return [id, fullName, age, hobbiesString, workInfoString];
+    }
+
+    reversedDayofTheWeek(day: string): number |string {
+        enum WeekDays {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+        }
+
+        const dayNumber: WeekDays | undefined = WeekDays[day as keyof typeof WeekDays];
+        return dayNumber !== undefined ? dayNumber : 'error';
+
+    }
 } 
--- 01.IntroductionToTypeScript/src/index.ts
+++ 01.IntroductionToTypeScript/src/index.ts
@@ -10,12 +10,40 @@ isEven = exercises.evenSum(2, 2, 3)
 console.log(isEven);
 
 /* 2. Day of the Week */
-let day = 1;
-let dayAsString = exercises.dayOfTheWeek(day);
+let dayAsString = exercises.dayOfTheWeek(1);
 console.log(dayAsString);
-day = 5;
-dayAsString = exercises.dayOfTheWeek(day);
+dayAsString = exercises.dayOfTheWeek(5);
 console.log(dayAsString);
-day = -1;
-dayAsString = exercises.dayOfTheWeek(day);
+dayAsString = exercises.dayOfTheWeek(-1);
 console.log(dayAsString);
+
+/* 3. Format Person */
+let personAsString = exercises.formatPerson(['Ivan', 20]);
+console.log(personAsString);
+personAsString = exercises.formatPerson(['Joro', 30]);
+console.log(personAsString);
+
+/* 4. Convert Array */
+let arr = ['Hello', ' ',  'Type', 'Script', '!'];
+let outputTuple: [string, number];
+outputTuple = exercises.convertArray(arr);
+console.log(outputTuple);
+
+/* 5. Convert Array with Map */
+let sumarized = 
+exercises.summarizePerson(12, 'Eliot', 'Des', 20, 'Braylen', ['tennis', 'football', 'hiking'], ['Sales Consultant', 2500]);
+console.log(sumarized);
+sumarized = exercises.summarizePerson(20, 'Mary', 'Trent', 25, undefined, ['fitness', 'rowing']);
+console.log(sumarized);
+sumarized = exercises.summarizePerson(21, 'Joseph', 'Angler', 28);
+console.log(sumarized);
+sumarized = exercises.summarizePerson(21, 'Kristine', 'Neva', 23, '');
+console.log(sumarized);
+
+/* 6. Reversed Day of the Week */
+let dayAsNumber = exercises.reversedDayofTheWeek('Monday');
+console.log(dayAsNumber);
+dayAsNumber = exercises.reversedDayofTheWeek('Friday');
+console.log(dayAsNumber);
+dayAsNumber = exercises.reversedDayofTheWeek('Invalid');
+console.log(dayAsNumber);
