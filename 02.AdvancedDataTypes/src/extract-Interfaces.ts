0c9e2cec356443b13da074d9fe9155c0f74cbc89
kiryazovkosta <kosta.kiryazov@gmail.com>
2025-05-27 09:13:02 +0300

    Advanced Data Types

---
+++ 02.AdvancedDataTypes/src/extract-Interfaces.ts
@@ -0,0 +1,81 @@
+
+
+type tasktype = {
+    status: 'Logged' | 'Started' | 'InProgress' | 'Done', 
+    title: string,
+    daysRequired: number,
+    assignedTo: {
+        username: string,
+        signupDate: Date
+    } | undefined,
+    changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done'): void
+}
+
+// function assignTask(
+//     user: { username: string, signupDate: Date },
+//     task: {
+//         status: 'Logged' | 'Started' | 'InProgress' | 'Done',
+//         title: string,
+//         daysRequired: number,
+//         assignedTo: {
+//             username: string,
+//             signupDate: Date
+//         } | undefined,
+//         changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done'): void
+//     }
+// ) {
+//     if (task.assignedTo == undefined) {
+//         task.assignedTo = user;
+//         console.log(`User ${user.username} assigned to task '${task.title}'`);
+//     }
+// }
+
+type UserType = user: {
+    username: string,
+    signupDate: Date,
+    passwordHash: string
+}
+
+function assignTask(user: usertype, task: tasktype) {
+    task: {
+        status: 'Logged' | 'Started' | 'InProgress' | 'Done',
+        title: string,
+        daysRequired: number,
+        assignedTo: {
+            username: string,
+            signupDate: Date
+        } | undefined,
+        changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done'): void
+    }
+) {
+    if (task.assignedTo == undefined) {
+        task.assignedTo = user;
+        console.log(`User ${user.username} assigned to task '${task.title}'`);
+    }
+}
+
+let user = {
+    username: 'Margaret',
+    signupDate: new Date(2022, 1, 13),
+    passwordHash: 'random'
+}
+
+let task1 = {
+    status: <'Logged' | 'Started' | 'InProgress' | 'Done'>'Logged',
+    title: 'Need assistance',
+    daysRequired: 1,
+    assignedTo: undefined,
+    changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done') {
+        this.status = newStatus;
+    }
+}
+
+let task2 = {
+    status: <'Logged' | 'Started' | 'InProgress' | 'Done'>'Done',
+    title: 'Test',
+    daysRequired: 12,
+    assignedTo: undefined, changeStatus(newStatus: 'Logged' | 'Started' | 'InProgress' | 'Done') { this.status = newStatus; }, moreProps: 300, evenMore: 'wow'
+} 
+
+assignTask(user, task1); 
+assignTask(user, task2);
